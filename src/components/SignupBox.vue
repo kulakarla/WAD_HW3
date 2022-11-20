59deed8daa25e1e473bada8a2833698608eee6c9
sandervalt <sandervalt@gmail.com>
2022-11-20 21:18:15 +0200

    SignupBox

--- src/components/SignupBox.vue
+++ src/components/SignupBox.vue
@@ -1,83 +1,147 @@
 <template>
-    <div class="signup">
-        <div id="alert" v-if="alert">{{ alert }}</div>
+  <div class="signup">
+    <div id="alert" v-if="alert">{{ alert }}</div>
 
-        <form @submit.prevent="signupWithPassword">
-            <label>
-                Email
-                <input type="email" v-model="email" />
-            </label>
-            <br>
-            <label>
-                Password
-                <input type="password" v-model="password" />
-            </label>
-            <button type="submit">Sign Up</button>
-        </form>
-    </div>
+    <form @submit.prevent="signupWithPassword">
+      <label class="emailLabel">
+        Email
+        <input placeholder="Email" class="emailAns" type="email" v-model="email" />
+      </label>
+      <br>
+      <label class="passLabel">
+        Password
+        <input placeholder="Password" class="passAns" type="password" v-model="password" />
+      </label>
+      <br>
+      <button class="button" onclick="signupWithPassword()" type="submit">Sign up
+        <span class="popuptext" id="myPopup">Invalid password. Password must be of length 8-14, start with an uppercase and include two lowercase alphabet characters, include the character '_' and include one numeric value</span>
+      </button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            email: "",
-            password: ""
-        };
+  data() {
+    return {
+      email: this.email,
+      password: this.password
+    };
+  },
+  methods: {
+    signupWithPassword() {
+      if (!this.passwordControl) {
+        var popup = document.getElementById("myPopup");
+        popup.classList.toggle("show")
+      }
     },
-    methods: {
-        signupWithPassword() {
-            this.alert = ""
-            if (this.passwordControl) {
-                this.alert = "You have been signed up"
-                return;
-            } else {
-                this.alert = "Invalid password. Password must be of length 8-14, start with an uppercase and include two lowercase alphabet characters, include the character '_' and include one numeric value"
-                return;
-            }
-        },
-        passwordControl() {
-            if (this.password.length < 8 || this.password.length > 14 || this.password[0] != this.password[0].toUpperCase) {
-                return false;
-            }
-            var consistsChar = false;
-            var consistsNumber = false;
-            let lowercase = 0;
-            for (let i = 0; i < this.password.length; i++) {
-                if (!isNaN(this.password[i])) {
-                    consistsNumber = true;
-                }
-                if (this.password[i] === this.password[i].toLowerCase) {
-                    lowercase += 1;
-                }
-                if (this.password[i] === "_") {
-                    consistsChar = true;
-                }
-            }
-            return consistsChar && consistsNumber && lowercase >= 2;
+    passwordControl() {
+      if (this.password.length < 8 || this.password.length > 14 || this.password[0] != this.password[0].toUpperCase) {
+        return false;
+      }
+      var consistsChar = false;
+      var consistsNumber = false;
+      let lowercase = 0;
+      for (let i = 0; i < this.password.length; i++) {
+        if (this.password[i] >= '0' && this.password[i] <= '9') {
+          consistsNumber = true;
         }
+        if (this.password[i] === this.password[i].toLowerCase) {
+          lowercase += 1;
+        }
+        if (this.password[i] === "_") {
+          consistsChar = true;
+        }
+      }
+      return consistsChar && consistsNumber && lowercase >= 2;
     }
+  }
 }
 
 </script>
 
 <style scoped>
-
 .signup{
-    border-radius: 25px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    background-color: #a3c4b7;
-    width: 65%;
-    flex-direction: row;
+  border-radius: 25px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #a3c4b7;
+  width: 65%;
+  flex-direction: row;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  height: 500px;
+  width: 1600px;
+  margin-left: 8%;
+}
+form {
+  margin: auto;
 }
 
-button{
-    display: flex;
-    background-color: #349eeb;
-    justify-content: center;
-    bottom: -10px;
+.button{
+  background-color: #349eeb;
+  font-size: 40px;
+  position: absolute;
+  bottom: 400px;
+  left: 880px;
+  border-radius: 25px;
+}
+.button:hover {
+  background-color: #94b8d6;
 }
 
+.emailLabel{
+  font-size: 70px;
+  padding-left: 160px;
+}
+.emailAns{
+  font-size: 30px;
+  right: 10%;
+  border-radius: 25px;
+}
+.passLabel{
+  font-size: 70px;
+  padding-left: 30px;
+}
+.passAns{
+  font-size: 30px;
+  border-radius: 25px;
+}
+
+.button .popuptext {
+  visibility: hidden;
+  width: 160px;
+  background-color: #555;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 8px 0;
+  position: absolute;
+  z-index: 1;
+  bottom: 125%;
+  left: 50%;
+  margin-left: -80px;
+}
+
+.button .popuptext::after {
+  content: "";
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: #555 transparent transparent transparent;
+}
+
+.button .show {
+  visibility: visible;
+  animation: fadeIn 1s
+}
+
+@keyframes fadeIn {
+  from {opacity: 0;}
+  to {opacity: 1;}
+}
 </style>
